aa24a9a73542854c5eb3e3ff8d9cc58180c16dcf
Daniel Sosa <jdsosa@gmail.com>
2017-08-17 20:36:36 -0400

    Change some pictures an gallery styles

--- views/gallery/index.php
+++ views/gallery/index.php
@@ -12,17 +12,19 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="row">
         <div class="col-md-6">
+            <h2 class="gallery-image-text"><?= Html::encode('Productos')?></h2>
             <?=
                 Html::a(
-                    '<div class="gallery-products m0a"><h2>' . Html::encode('Productos') . '</h2></div>',
+                    '<div class="gallery-products m0a"><span></span></div>',
                     ['gallery/products']
                 );
             ?>
         </div>
         <div class="col-md-6">
+            <h2 class="gallery-image-text"><?= Html::encode('Eventos')?></h2>
             <?=
             Html::a(
-                '<div class="gallery-events m0a"><h2>' . Html::encode('Eventos') . '</h2></div>',
+                '<div class="gallery-events m0a"><span></span></div>',
                 ['gallery/events']
             );
             ?>
