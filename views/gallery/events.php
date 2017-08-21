5068097f64b95bdaea4010fc9e5750170ce347fe
Daniel Sosa <jdsosa@gmail.com>
2017-08-21 14:58:25 -0400

    Added a gallery of events

--- views/gallery/events.php
+++ views/gallery/events.php
@@ -10,6 +10,24 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-gallery">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <h2>Eventos</h2>
+    <div class="row">
+        <?php for ($i = 1; $i < 11; $i++) : ?>
+            <div class="col-md-3">
+                <div class="view hm-zoom">
+                    <?= Html::a(
+                        Html::img(Yii::getAlias('@web') . '/images/gallery/events/thumb-' . $i . '.jpg', [
+                            'class' => 'img-responsive',
+                            'alt'   => Html::encode('Delishots Desserts'),
+                        ]),
+                        Yii::getAlias('@web') . '/images/gallery/events/full-' . $i . '.jpg',
+                        [
+                            'data-fancybox' => "gallery",
+                            'data-caption'  => "Delishots Desserts",
+                        ]
+                    )?>
+                </div>
+            </div>
+        <?php endfor; ?>
+    </div>
 
 </div>
